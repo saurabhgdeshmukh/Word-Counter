98461c2af3c62318299ae7f16ee70556ab6c7e46
saurabhgdeshmukh <saurabhgdeshmukh@gmail.com>
2023-10-04 23:46:01 +0530

    Dark mode changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+ 
 import './App.css';
-
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+ import React, { useState } from 'react';
+ 
 function App() {
+  const[mode,setMode]=useState('light');
+ const togglemode= ()=>{
+  if(mode==='light')
+  {
+    setMode('dark')
+    document.body.style.backgroundColor='black'
+  }
+  else
+  {
+    setMode('light')
+    document.body.style.backgroundColor='white'
+  }
+ }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
+    {/* <Navbar/> */}
+    <Navbar title="TextUtils" mode={mode} togglemode={togglemode} />
+    <div className="container my-3">
+    <TextForm heading="Enter the text to analyze below"/>
     </div>
+    </> 
   );
 }
 
